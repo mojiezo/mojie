725fc24d64efe87f233e1763fba43daa9138dd73
mojiezo <166317951+mojiezo@users.noreply.github.com>
2024-11-29 12:04:12 +0800

    Update mo.js

--- 網站/mo.js
+++ 網站/mo.js
@@ -1,15 +1,16 @@
+// 等待 DOM 完全加載
 document.addEventListener("DOMContentLoaded", function() {
-    const textElement = document.querySelector('.a p');
-    setTimeout(function() {
-      textElement.classList.add('show');
-    }, 2000);
-  });
-document.addEventListener("DOMContentLoaded", function () {
-    const menuButton = document.querySelector(".menu");
-    const navMenu = document.querySelector(".nav-menu");
+  // 文字淡入效果
+  const textElement = document.querySelector('.a p');
+  setTimeout(function() {
+    textElement.classList.add('show');
+  }, 2000);
 
-    // 點擊漢堡按鈕，切換選單顯示/隱藏
-    menuButton.addEventListener("click", function () {
-        navMenu.classList.toggle("show");
-    });
+  // 漢堡菜單顯示/隱藏
+  const menuButton = document.querySelector(".menu");
+  const navMenu = document.querySelector(".nav-menu");
+
+  menuButton.addEventListener("click", function() {
+    navMenu.classList.toggle("show");
+  });
 });
